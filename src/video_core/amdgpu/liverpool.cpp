a0cbbe64aee5ddbafc996d882236fa9e288ceba7
jute-ado <304153664+jute-ado@users.noreply.github.com>
2026-07-26 02:23:40 +0200

    video_core: make RenderDoc IPC capture reliable

--- src/video_core/amdgpu/liverpool.cpp
+++ src/video_core/amdgpu/liverpool.cpp
@@ -103,8 +103,6 @@ void Liverpool::Process(std::stop_token stoken) {
             break;
         }
 
-        VideoCore::StartCapture();
-
         curr_qid = -1;
 
         while (num_submits || num_commands) {
@@ -122,6 +120,7 @@ void Liverpool::Process(std::stop_token stoken) {
                 }
                 task = queue.submits.front();
             }
+            VideoCore::StartCapture();
             task.resume();
 
             if (task.done()) {
---
+++ src/video_core/renderdoc_capture_state.h
@@ -0,0 +1,37 @@
+// SPDX-FileCopyrightText: Copyright 2026 shadPS4 Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include <atomic>
+
+namespace VideoCore {
+
+class RenderDocCaptureState {
+    enum class State {
+        Idle,
+        Triggered,
+        InProgress,
+    };
+
+public:
+    [[nodiscard]] bool Trigger() {
+        auto expected = State::Idle;
+        return state.compare_exchange_strong(expected, State::Triggered);
+    }
+
+    [[nodiscard]] bool BeginSubmission() {
+        auto expected = State::Triggered;
+        return state.compare_exchange_strong(expected, State::InProgress);
+    }
+
+    [[nodiscard]] bool EndSubmission() {
+        auto expected = State::InProgress;
+        return state.compare_exchange_strong(expected, State::Idle);
+    }
+
+private:
+    std::atomic<State> state{State::Idle};
+};
+
+} // namespace VideoCore
