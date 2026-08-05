b5290374c61be071ee2cd531d46ceb03de72bdab
jute-ado <304153664+jute-ado@users.noreply.github.com>
2026-08-05 14:22:44 +0200

    tests: reproduce uncommitted zeroed stream allocations

---
+++ src/video_core/buffer_cache/stream_buffer_upload.h
@@ -0,0 +1,19 @@
+// SPDX-FileCopyrightText: Copyright 2026 shadPS4 Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include <cstring>
+
+#include "common/types.h"
+
+namespace VideoCore {
+
+template <typename StreamBuffer>
+u64 AllocateZeroedStreamRegion(StreamBuffer& buffer, u64 size, u64 alignment) {
+    const auto [data, offset] = buffer.Map(size, alignment);
+    std::memset(data, 0, size);
+    return offset;
+}
+
+} // namespace VideoCore
