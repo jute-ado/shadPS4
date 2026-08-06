71b7e58920b38adfdad9506e08497e319fff9844
jute-ado <304153664+jute-ado@users.noreply.github.com>
2026-08-07 00:38:45 +0200

    video_core: scope GPU event preparation to physical owners

--- src/video_core/buffer_cache/buffer_residency.h
+++ src/video_core/buffer_cache/buffer_residency.h
@@ -26,6 +26,13 @@ template <typename TransitionTextures, typename RetireBufferOwners, typename Syn
            std::invoke(std::forward<SynchronizeWritebacks>(synchronize_writebacks));
 }
 
+template <typename PreparePhysicalOwnership>
+[[nodiscard]] bool PrepareGpuEventMemoryWrite(bool has_imported_physical_backing,
+                                              PreparePhysicalOwnership&& prepare_physical) {
+    return !has_imported_physical_backing ||
+           std::invoke(std::forward<PreparePhysicalOwnership>(prepare_physical));
+}
+
 enum class PhysicalBackingTextureProducer {
     Copy,
     ComputeShader,
--- src/video_core/renderer_vulkan/vk_rasterizer.cpp
+++ src/video_core/renderer_vulkan/vk_rasterizer.cpp
@@ -1320,10 +1320,9 @@ bool Rasterizer::ReadMemory(VAddr addr, u64 size) {
 }
 
 bool Rasterizer::PrepareGpuEventMemoryWrite(VAddr addr, u64 size) {
-    if (!physical_backing_coordinator || !IsMapped(addr, size)) {
-        return true;
-    }
-    return InvalidateMemory(addr, size);
+    return VideoCore::PrepareGpuEventMemoryWrite(
+        physical_backing_coordinator != nullptr,
+        [this, addr, size] { return buffer_cache.PreparePhysicalBackingHostWrite(addr, size); });
 }
 
 void Rasterizer::ProcessDownloadImages() {
