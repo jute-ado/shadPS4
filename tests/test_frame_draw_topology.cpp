036fe2105da42c1a8c0337e792b2946716793431
jute-ado <304153664+jute-ado@users.noreply.github.com>
2026-08-07 09:38:32 +0200

    tests: fingerprint ordered draw submission packets

--- tests/test_frame_draw_topology.cpp
+++ tests/test_frame_draw_topology.cpp
@@ -11,6 +11,11 @@ namespace {
 TEST(FrameDrawTopologyDiagnostic, SnapshotsAndClearsBoundedFrameCounters) {
     FrameDrawTopologyDiagnostic tracker{2};
 
+    tracker.ObservePacket(DrawTopologyPacket::DrawIndex2);
+    tracker.ObservePacket(DrawTopologyPacket::DrawIndirect);
+    tracker.ObservePacket(DrawTopologyPacket::SetBase);
+    tracker.ObservePacket(DrawTopologyPacket::IndexBufferSize);
+    tracker.ObservePacket(DrawTopologyPacket::SetPredication);
     tracker.ObserveDraw(DrawTopologyKind::Direct);
     tracker.ObserveDraw(DrawTopologyKind::DirectIndexed);
     tracker.ObserveDraw(DrawTopologyKind::Indirect);
@@ -37,6 +42,10 @@ TEST(FrameDrawTopologyDiagnostic, SnapshotsAndClearsBoundedFrameCounters) {
     EXPECT_EQ(first.occlusion_control, 1);
     EXPECT_EQ(first.occlusion_dump, 1);
     EXPECT_EQ(first.occlusion_reset, 1);
+    EXPECT_EQ(first.set_base, 1);
+    EXPECT_EQ(first.index_buffer_size, 1);
+    EXPECT_EQ(first.set_predication, 1);
+    EXPECT_NE(first.packet_hash, FrameDrawTopologyDiagnostic::EmptyPacketHash());
 
     const auto second = tracker.TakeSnapshot();
     EXPECT_EQ(second.sequence, 2);
@@ -45,10 +54,32 @@ TEST(FrameDrawTopologyDiagnostic, SnapshotsAndClearsBoundedFrameCounters) {
     EXPECT_EQ(second.submitted + second.filtered + second.missing_pipeline +
                   second.binding_failed,
               0);
+    EXPECT_EQ(second.packet_hash, FrameDrawTopologyDiagnostic::EmptyPacketHash());
 
     const auto third = tracker.TakeSnapshot();
     EXPECT_EQ(third.sequence, 3);
     EXPECT_FALSE(third.should_report);
+}
+
+TEST(FrameDrawTopologyDiagnostic, OrderedPacketFingerprintChangesWithTopology) {
+    FrameDrawTopologyDiagnostic first_tracker{1};
+    FrameDrawTopologyDiagnostic same_tracker{1};
+    FrameDrawTopologyDiagnostic changed_tracker{1};
+
+    for (auto* tracker : {&first_tracker, &same_tracker}) {
+        tracker->ObservePacket(DrawTopologyPacket::DrawIndex2);
+        tracker->ObservePacket(DrawTopologyPacket::SetBase);
+        tracker->ObservePacket(DrawTopologyPacket::DrawIndirect);
+    }
+    changed_tracker.ObservePacket(DrawTopologyPacket::DrawIndex2);
+    changed_tracker.ObservePacket(DrawTopologyPacket::IndexBufferSize);
+    changed_tracker.ObservePacket(DrawTopologyPacket::DrawIndirect);
+
+    const auto first = first_tracker.TakeSnapshot();
+    const auto same = same_tracker.TakeSnapshot();
+    const auto changed = changed_tracker.TakeSnapshot();
+    EXPECT_EQ(first.packet_hash, same.packet_hash);
+    EXPECT_NE(first.packet_hash, changed.packet_hash);
 }
 
 } // namespace
