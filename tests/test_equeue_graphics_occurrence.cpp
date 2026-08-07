c220eb61dd72d20c7422052fea0d808136c9ebc0
jute-ado <304153664+jute-ado@users.noreply.github.com>
2026-08-07 04:05:20 +0200

    test: drain graphics occurrences fairly

--- tests/test_equeue_graphics_occurrence.cpp
+++ tests/test_equeue_graphics_occurrence.cpp
@@ -1,6 +1,8 @@
 // SPDX-FileCopyrightText: Copyright 2026 shadPS4 Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#include <array>
+#include <vector>
 #include <gtest/gtest.h>
 
 #include "core/libraries/kernel/equeue.h"
@@ -43,4 +45,62 @@ TEST(EqueueGraphicsOccurrence, OrdinaryClearEventsStillCoalesceRepeatedTriggers)
 
     EXPECT_FALSE(event.IsTriggered());
     EXPECT_EQ(event.event.data, 0u);
+}
+
+TEST(EqueueGraphicsOccurrence, ReadyGraphicsIdsShareCapacityBeforeRepeatedOccurrences) {
+    EqueueEvent eop{};
+    eop.event.ident = 0x40;
+    eop.event.filter = OrbisKernelEvent::Filter::GraphicsCore;
+    eop.event.flags = OrbisKernelEvent::Flags::Clear;
+    eop.Trigger(reinterpret_cast<void*>(0x40));
+    eop.Trigger(reinterpret_cast<void*>(0x40));
+    eop.Trigger(reinterpret_cast<void*>(0x40));
+
+    EqueueEvent compute{};
+    compute.event.ident = 0x1;
+    compute.event.filter = OrbisKernelEvent::Filter::GraphicsCore;
+    compute.event.flags = OrbisKernelEvent::Flags::Clear;
+    compute.Trigger(reinterpret_cast<void*>(0x1));
+
+    std::vector<EqueueEvent> events;
+    events.emplace_back(std::move(eop));
+    events.emplace_back(std::move(compute));
+
+    std::array<OrbisKernelEvent, 2> delivered{};
+    ASSERT_EQ(Libraries::Kernel::DrainReadyEvents(events, delivered.data(), 2), 2);
+    EXPECT_EQ(delivered[0].ident, 0x40u);
+    EXPECT_EQ(delivered[1].ident, 0x1u);
+
+    ASSERT_EQ(Libraries::Kernel::DrainReadyEvents(events, delivered.data(), 2), 2);
+    EXPECT_EQ(delivered[0].ident, 0x40u);
+    EXPECT_EQ(delivered[1].ident, 0x40u);
+    EXPECT_EQ(Libraries::Kernel::DrainReadyEvents(events, delivered.data(), 2), 0);
+}
+
+TEST(EqueueGraphicsOccurrence, RemainingCapacityDrainsRepeatedGraphicsOccurrences) {
+    EqueueEvent eop{};
+    eop.event.ident = 0x40;
+    eop.event.filter = OrbisKernelEvent::Filter::GraphicsCore;
+    eop.event.flags = OrbisKernelEvent::Flags::Clear;
+    eop.Trigger(reinterpret_cast<void*>(0x40));
+    eop.Trigger(reinterpret_cast<void*>(0x40));
+    eop.Trigger(reinterpret_cast<void*>(0x40));
+
+    EqueueEvent compute{};
+    compute.event.ident = 0x1;
+    compute.event.filter = OrbisKernelEvent::Filter::GraphicsCore;
+    compute.event.flags = OrbisKernelEvent::Flags::Clear;
+    compute.Trigger(reinterpret_cast<void*>(0x1));
+
+    std::vector<EqueueEvent> events;
+    events.emplace_back(std::move(eop));
+    events.emplace_back(std::move(compute));
+
+    std::array<OrbisKernelEvent, 4> delivered{};
+    ASSERT_EQ(Libraries::Kernel::DrainReadyEvents(events, delivered.data(), 4), 4);
+    EXPECT_EQ(delivered[0].ident, 0x40u);
+    EXPECT_EQ(delivered[1].ident, 0x1u);
+    EXPECT_EQ(delivered[2].ident, 0x40u);
+    EXPECT_EQ(delivered[3].ident, 0x40u);
+    EXPECT_EQ(Libraries::Kernel::DrainReadyEvents(events, delivered.data(), 1), 0);
 }
